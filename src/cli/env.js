2d5d41b6639150e0ffb7bf9746f936fea472abff
lysenko-alex <alex91113011@gmail.com>
2025-10-26 11:36:08 +0200

    feat(cli): implement environment variable parsing

--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,17 @@
+const ENV_PREFIX = 'RSS_';
+
 const parseEnv = () => {
-  // Write your code here
+  try {
+    const env = process.env;
+    const envKeys = Object.keys(env).filter(key => key.startsWith(ENV_PREFIX));
+    const envValues = envKeys.map(key => `${key}=${env[key]}`);
+    console.log(envValues.join('; '));
+    if (envValues.length === 0) {
+      console.log('No valid environment variables found');
+    }
+  } catch (error) {
+    throw new Error(error);
+  }
 };
 
 parseEnv();
