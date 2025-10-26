5e15572fb98f3a7ca6f5629e3d89a1737899a85d
lysenko-alex <alex91113011@gmail.com>
2025-10-26 11:34:23 +0200

    feat(fs): implement file creation functionality

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,21 @@
+import fs from 'node:fs/promises';
+import { checkFileExists } from '../shared.js';
+
+const FILE_NAME = 'fresh.txt';
+const FILE_CONTENT = 'I am fresh and young';
+const FILE_PATH = `./src/fs/files/${FILE_NAME}`;
+
+
 const create = async () => {
-  // Write your code here
+  try {
+    const fileExists = await checkFileExists(FILE_PATH);
+    if (fileExists) {
+      throw new Error('FS operation failed');
+    }
+    await fs.writeFile(FILE_PATH, FILE_CONTENT);
+  } catch (error) {
+    throw new Error(error);
+  }
 };
 
 await create();
