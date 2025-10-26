708ad9b8555b141cc0566a3bb22ab3afc19a78c3
lysenko-alex <alex91113011@gmail.com>
2025-10-26 11:34:41 +0200

    feat(fs): implement directory copying functionality

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,5 +1,40 @@
+import fs from 'node:fs/promises';
+import { ERROR_MESSAGE, checkFileExists } from '../shared.js';
+
+const FILES_PATH = './src/fs/files';
+const FILES_COPY_PATH = './src/fs/files_copy';
+
+const getFiles = async (path) => {
+  try {
+    const files = await fs.readdir(path, { withFileTypes: true });
+    return files.filter(file => file.isFile());
+  } catch (error) {
+    console.log(error);
+    return [];
+  }
+};
+
 const copy = async () => {
-  // Write your code here
+  try {
+    const files = await getFiles(FILES_PATH);
+    const copyDirectoryExists = await checkFileExists(FILES_COPY_PATH);
+                                        
+    if (copyDirectoryExists) {
+      throw new Error(ERROR_MESSAGE);
+    }
+    await fs.mkdir(FILES_COPY_PATH, { recursive: true })
+            .catch(() => { throw new Error(ERROR_MESSAGE); });
+
+    for (const file of files) {
+      const filePath = `${FILES_PATH}/${file.name}`;
+      const fileContent = await fs.readFile(filePath, 'utf-8');
+      await fs.writeFile(`${FILES_COPY_PATH}/${file.name}`, fileContent);
+    }
+
+  } catch(error) {
+    console.log(error);
+    throw new Error(error);
+  }
 };
 
 await copy();
