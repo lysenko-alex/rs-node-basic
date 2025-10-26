b80f55a9b1d9ddb2c3ccbf46b36df0d71c4d0300
lysenko-alex <alex91113011@gmail.com>
2025-10-26 11:36:32 +0200

    feat(cli): implement command line argument parsing

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,25 @@
+const ARG_PREFIX = '--';
+
+const formatArgs = ([key, value]) => `${key} is ${value}`;
+
 const parseArgs = () => {
-  // Write your code here
+  try {
+    const args = process.argv.slice(2);
+    const parsedArgs = args.reduce((acc, arg, index) => {
+      if (arg && arg.startsWith(ARG_PREFIX) && args[index + 1]) {
+        const propName = arg.substring(ARG_PREFIX.length);
+        const value = args[index + 1];
+        acc[propName] = value;
+      }
+      return acc;
+    }, {});
+    
+    const formattedOutput = Object.entries(parsedArgs).map(formatArgs).join(', ');
+    
+    console.log(formattedOutput);
+  } catch (error) {
+    throw new Error(error);
+  }
 };
 
 parseArgs();
