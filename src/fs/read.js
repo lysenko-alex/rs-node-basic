cd7317458d52b35776415e3202bd4a2b0b87a052
lysenko-alex <alex91113011@gmail.com>
2025-10-26 11:35:47 +0200

    feat(fs): implement file reading functionality

--- src/fs/read.js
+++ src/fs/read.js
@@ -1,5 +1,20 @@
+import fs from 'node:fs/promises';
+import { ERROR_MESSAGE, checkFileExists } from '../shared.js';
+
+const FILE_PATH = './src/fs/files/fileToRead.txt';
+
 const read = async () => {
-  // Write your code here
+  try {
+    const fileExists = await checkFileExists(FILE_PATH);
+    if (!fileExists) {
+      throw new Error(ERROR_MESSAGE);
+    }
+    const fileContent = await fs.readFile(FILE_PATH, 'utf-8');
+    console.log(fileContent);
+  } catch (error) {
+    console.error(error.message);
+    throw new Error(ERROR_MESSAGE);
+  }
 };
 
 await read();
