ac5a5d3781d42cbffe04fbd5aa504b5c8693d138
lysenko-alex <alex91113011@gmail.com>
2025-10-26 11:38:21 +0200

    feat(streams): implement transform stream for text reversal

--- src/streams/transform.js
+++ src/streams/transform.js
@@ -1,5 +1,20 @@
+import { pipeline } from 'node:stream/promises';
+import { Transform } from 'node:stream';
+
+
 const transform = async () => {
-  // Write your code here
+  const reverseTransform = new Transform({
+    transform(chunk, _, callback) {
+      this.push(chunk.toString().split('').reverse().join('') + '\n');
+      callback();
+    }
+  });
+
+  pipeline(
+    process.stdin,
+    reverseTransform,
+    process.stdout
+  );
 };
 
 await transform();
